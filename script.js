33a77acb49462a2e29cda6613511f37472340fe3
Agent-D14 <136282969+Agent-D14@users.noreply.github.com>
2023-09-03 19:03:14 -0500

    Popup Function added

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.menu');
-
-    burger.addEventListener('click', () =>{
-        nav.classList.toggle('nav-active');
-    });
-}
-
-navSlide();
+var closePopup = document.getElementById("popupclose");
+    var closePopup2 = document.getElementById("popupclose2");
+	var popup = document.getElementById("popup");
+	var button = document.getElementById("button");
+    var button2 = document.getElementById("signin-btn");
+	// Close Popup Event
+	closePopup.onclick = function() {
+	  popup.style.display = 'none';
+      popup2.style.display = 'none';
+	};
+    closePopup2.onclick = function() {
+	  popup.style.display = 'none';
+      popup2.style.display = 'none';
+	};
+	// Show Overlay and Popup
+	button.onclick = function() {
+	  popup.style.display = 'block';
+	}
+    button2.onclick = function(){
+        popup2.style.display = 'block';
+    }
